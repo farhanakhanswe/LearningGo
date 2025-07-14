d66c8f601bfed4d94e913ba057d84074b1e6755a
farhanakhanswe <farhanakhanswe@gmail.com>
2025-07-14 14:46:11 +0600

    info about print function

--- app.go
+++ app.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("Hello World");
+	//	Print is a function provided by built-in "fmt" package
+
+	fmt.Print("Hello World\n")
+	fmt.Print(`Hello World with Back Ticks!`)
+
+	/* fmt.Print('Hello World') -> This will cause error as strings need to be either wrapped with double quotes
+	or back ticks
+	*/
 }
+
